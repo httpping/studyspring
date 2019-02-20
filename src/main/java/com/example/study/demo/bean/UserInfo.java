2bf7f736c748d6f0914de2735ca2d38783caa2ae
xiaoliang <xiaoliang@globalegrow.com>
2019-02-20 17:07:05 +0800

    提交bootstrap

---
+++ src/main/java/com/example/study/demo/bean/UserInfo.java
@@ -0,0 +1,34 @@
+package com.example.study.demo.bean;
+
+import lombok.Data;
+
+@Data
+public class UserInfo {
+    public String name;
+    public Integer age;
+    public String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
--- src/main/java/com/example/study/demo/controler/RecordController.java
+++ src/main/java/com/example/study/demo/controler/RecordController.java
@@ -3,18 +3,25 @@ package com.example.study.demo.controler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import com.example.study.demo.bean.UserInfo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RecordController {
 
-    @GetMapping("/record")
+    @GetMapping(value = "/record")
     public String recordIndex (Model model) {
         model.addAttribute("host", "http://blog.didispace.com");
         return "/record";
     }
 
-    @GetMapping(value = "/commit")
-    public String commit (Model model) {
+    @PostMapping(value = "/commit")
+    public String commit (UserInfo userInfo, Model model) {
+        model.addAttribute("name", userInfo.name);
+        model.addAttribute("age", userInfo.age);
+        model.addAttribute("sex",userInfo.sex);
         return "/success";
     }
 }
